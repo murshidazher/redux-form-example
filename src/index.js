d8e8a3e8da9f165db137d1b950037ca653e0c731
murshidazher <murshidazher@yahoo.com>
2020-06-19 01:44:53 +0530

    feat(form): create a simple redux-form

--- src/index.js
+++ src/index.js
@@ -15,4 +15,9 @@ let store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
