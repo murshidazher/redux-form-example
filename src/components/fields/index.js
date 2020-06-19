1ab22fc49e266021269796f31891d17726ab744c
murshidazher <murshidazher@yahoo.com>
2020-06-19 12:18:26 +0530

    feat(meta): view state of field using props meta

--- src/components/fields/index.js
+++ src/components/fields/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ReactJson from "react-json-view";
 
 export const customInput = props => {
   return (
     <div>
       <label>{props.label}</label>
       <input {...props.input} type={props.type} />
+      <ReactJson src={props.meta} />
     </div>
   );
 };
@@ -14,10 +16,10 @@ export const customSelect = props => {
     <div>
       <label>{props.label}</label>
       <select {...props.input}>
-        <option />
         <option value="tabs">Tabs</option>
         <option value="spaces">Spaces</option>
       </select>
+      <ReactJson src={props.meta} />
     </div>
   );
 };
